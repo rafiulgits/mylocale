913b3dab060f8e997623ae8d7342d98ae6a2d083
diptomondal007 <diptomondal007@gmail.com>
2019-04-19 18:50:39 +0600

    Profile completed

---
+++ staticfiles/js/account/manage/profilejs.js
@@ -0,0 +1,17 @@
+
+
+function showIssueCreated() {
+    document.getElementById("issue-created-by-me-div").style.display = 'block';
+    document.getElementById("issue-progressing-div").style.display = 'none';
+
+    document.getElementById("issues-created-by-me").classList.add("list-group-item-info");
+    document.getElementById("issues-progressing").classList.remove("list-group-item-info");
+
+}
+
+function showIssueProgressing() {
+    document.getElementById("issue-created-by-me-div").style.display = 'none';
+    document.getElementById("issue-progressing-div").style.display = 'block';
+    document.getElementById("issues-progressing").classList.add("list-group-item-info");
+    document.getElementById("issues-created-by-me").classList.remove("list-group-item-info");
+}
